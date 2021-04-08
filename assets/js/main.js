57e5f051757f356d3000cb81ef3227665e8c83b6
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-04-08 16:28:15 +0200

    completo con bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
-// SCOMMENTARE SE SI USA JQUERY
-// $(document).ready(function(){
-//
-// });
-
-// SCOMMENTARE SE SI USA VUE
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello Vue!'
+    mails: [],
+  },
+  methods: {
+    getMail: function(){
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          let mail = response.data.response;
+          this.mails.push(mail);
+        });
+      }
+      console.log(this.mails);
+    }
   }
 })
